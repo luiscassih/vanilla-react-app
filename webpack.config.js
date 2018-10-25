00eecd14228cedcd38536b23f6802d0a6eb8706c
Luis Cassih <luiscassih@protonmail.com>
2018-10-25 13:34:43 -0300

    Add file-loader and babel experimental class support

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,18 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         filename: 'app-bundle.js'
     },
+    devServer: {
+        port: 8080,
+        historyApiFallback: true
+    },
     module: {
         rules: [
             { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader'] },
-            { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' }
+            { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
+            { test: /\.(jpg|png|gif|svg|pdf|ico)$/, use: [
+                    { loader: 'file-loader', options: { name: '[path][name]-[hash:8].[ext]'}}
+                ]
+            }
         ]
     },
     mode: 'development',
